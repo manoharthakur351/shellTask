062fc99f71a43ad401b90f45041a43c6ca8abb70
redRaven <tsmile2459@gmail.com>
2023-01-24 17:37:12 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   .gitignore
    	new file:   gitit
    	modified:   main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import mysql.connector
 import os
 import hashlib
 import configparser
+from prettytable import PrettyTable
 
 # some basic initialisations
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("config.ini")
+
 
 class task:
-
     STATUS = None
     PERCENT = None
     created_on = None
@@ -20,13 +21,34 @@ class task:
     def __init__(self):
         return None
 
-    #
+
+class User:
+    name = None
+    id = None
+    goals = []
+
+    # constructor
+    def __init__(self):
+        pass
+
+    # print goals
+    def print_goals(self, cursor):
+        table = PrettyTable(["id", "goal", "dead line"])
+        # printing all goals
+        cursor.execute("SELECT * FROM goals")
+        for x in cursor:
+            table.add_row(list(x))
+        print(table)
+        return
 
 
 class Goal:
-    DEADLINE = None
+    dead = None
+    name = None
 
-    pass
+    # constructor
+    def __init__(self):
+        pass
 
 
 class Day:
@@ -34,28 +56,47 @@ class Day:
     pass
 
 
-# functions
-querry1 = """
-CREATE TABLE test1 (                                          sno int (5) ,
-name varchar (50) not null,
-deadline datetime not null,
-taskid int (5) not null,
-primary key (sno)
-)
-"""
+# FUNCTIONS--------------------------------------------------------------------------------------------------------------
 
+# delete goal
+def delete_goal(cursor, conn):
+    goal_id = input("\t enter goal id you wanna delete >_ ")
+    q = f"DELETE FROM goals WHERE goalid='{goal_id}'"
+    try:
+        cursor.execute(q)
+        conn.commit()
+    except:
+        print("[!] ERROR! unable to delete goal.")
+    return
+
+
+def newgoal(cursor, conn):
+    name = input("\t name: ")
+    dead = input("\t deadline: ")
+    q = f"insert into goals (name, deadline) values ('{name}','{dead}')"
+    try:
+        cursor.execute(q)
+        conn.commit()
+        return "success"
+    except:
+        return "faliure"
+
+
+# clean screen with logo
 def clear():
     os.system("clear")
     os.system("echo WELCOME TO")
     os.system("figlet shellTask")
 
+
+# authentication
 def authentic(cursor):
     username = input("username: ")
     pass_str = input("password: ")
     querry = (
         f"select * from users where username='{username}' and password='{pass_str}'"
     )
-    #print(querry)
+    # print(querry)
     cursor.execute(querry)
     result = cursor.fetchone()
     if result:
@@ -67,49 +108,73 @@ def authentic(cursor):
         authentic(cursor)
 
 
-
 # Main
 def main():
     clear()
+
+    # initialisations
+    table = PrettyTable(["id", "goal", "dead line"])
+
+    # connection object
     mydb = mysql.connector.connect(
-        host=config['DATABASE']['host'], user="u0_a74", password="0000", database="shelltask"
+        host=config["DATABASE"]["host"],
+        user="u0_a74",
+        password="0000",
+        database="shelltask",
     )
 
     mycursor = mydb.cursor()
 
     # authenticate
     authentic(mycursor)
+    user = User()
+    clear()
+
+    # mainloop
     while True:
         clear()
+
+        # options
+        user.print_goals(mycursor)
         print("[ ] Choose a operation:")
-        print("  01: List goals")
-        print("  02: New goal")
-        print("  03: exit")
-        print("  04: other.....")
-        print("  05: list tables in shelltask")
+        print("  01: new goal")
+        print("  02: remove goal")
+        print("  03: view goal")
+        print("  04: edit goal")
+        print()
+
         choice = input("[*] Choice >_ ")
 
+        # 1 adding new goal
         if choice == "01":
-            clear()
-            print("[ ] Here os list of goals:-")
-            mycursor.execute("SELECT * FROM goals")
-            for x in mycursor:
-                print(x)
+            o = newgoal(mycursor, mydb)
+            input(o)
 
-            input()
+        # 2 remove goal
+        if choice == "02":
+            delete_goal(mycursor, mydb)
 
+        # 3 view goal
+        if choice == "03":
+            pass
+
+        # 4 edit goal
+        if choice == "04":
+            pass
+
+        # test
         if choice == "05":
-            clear()
             mycursor.execute("SHOW tables")
             for x in mycursor:
                 print(x)
             input()
 
-        if choice == "":
-            clear()
+        # exit
+        if choice == r"/":
             print("[ ] Terminating Session")
+            mycursor.close()
+            mydb.commit()
             mydb.close()
-
             exit()
 
 
