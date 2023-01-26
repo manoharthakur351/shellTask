bdc11d00b09142c3f3d0143be90191440ed8e63a
redRaven <tsmile2459@gmail.com>
2023-01-26 16:34:51 +0530

    new file:   proplot.py

---
+++ proplot.py
@@ -0,0 +1,34 @@
+#!/bin/python
+import shutil
+
+# basic iniatialisation
+rows, columns = shutil.get_terminal_size()
+print(rows, columns)
+
+# CONSTANTS
+LABELXPAD=1
+LABELYPAD=3
+
+# TEST DATA
+x = (1,2,3,4,5,6)
+y = (3,8,5,9,4,9)
+
+# FUNCTIONS
+# data_valid: check wheather data is valid or not
+def data_valid(x = None, y = None, xy = None):
+    if xy == None :
+        print()
+# MAIN
+def proplot ():
+    pass
+
+
+
+proplot()
+
+
+# TEST CODE
+
+a , b = input("a: "), input("b: ")
+ab = ((1,2),(2,3))
+print(data_valid(x=a,y=b))
